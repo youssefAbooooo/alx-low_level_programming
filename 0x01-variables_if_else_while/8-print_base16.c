71c37c2902e3b963fce0f434a54b5c56d49d32fb
youssefAbooooo <youssef@example.com>
2023-09-11 06:24:36 +0300

    update

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print the numbers from 1 to 10.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	for (i = 'a'; i <= 'f'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
