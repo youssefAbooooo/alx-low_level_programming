e1db234eef53fceb56bdfe19fb453ff600bfb1a0
youssefAbooooo <youssef@example.com>
2023-09-11 05:15:06 +0300

    ddj

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (char i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
