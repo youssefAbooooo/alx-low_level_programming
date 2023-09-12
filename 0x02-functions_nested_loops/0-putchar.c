d03c34ab72126ee8ba78d4ecdd42a8fd87d22aed
youssefAbooooo <youssef@example.com>
2023-09-12 14:02:03 -0700

    last

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,19 +2,19 @@
 #include "main.h"
 
 /**
- * main entry point
+ * main - Entry point
  *
- * prints _putchar using putchar prototype
+ * Descreption: prints _putchar using putchar prototype
  *
- * return: always 0 (success)
- */
+ * Return: Always 0 (Success)
+*/
 
 int main(void)
 {
 	char str[] = "_putchar";
 	int i;
 
-	for (i = 0 ; i < 8 ; i++ )
+	for (i = 0 ; i < 8 ; i++)
 		_putchar(str[i]);
 	_putchar('\n');
 	return (0);
