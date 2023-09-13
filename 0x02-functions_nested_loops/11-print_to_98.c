27b062694fb0564e9b50a151952c82fa7e67ca13
youssefAbooooo <youssef@example.com>
2023-09-12 17:50:36 -0700

    j

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,9 +8,9 @@
  */
 void print_to_98(int n)
 {
-	if (n >= 98)
+	if (n <= 98)
 	{
-		for (; n > 97; n++)
+		for (; n <= 98; n++)
 		{
 			if (n < 10 && n > -10)
 				_putchar(n % 10 + '0');
@@ -28,7 +28,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-		for (; n < 99; n--)
+		for (; n >= 98; n--)
 		{
 			_putchar(n / 10 + '0');
 			_putchar(n % 10 + '0');
