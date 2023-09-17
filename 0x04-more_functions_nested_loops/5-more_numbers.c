c22202be129daa023b0ca4f742ea7ba52d8156cc
youssefAbooooo <youssef@example.com>
2023-09-17 08:40:53 -0700

    kj

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -20,8 +20,8 @@ void more_numbers(void)
 		for (j = 0; j < 15; j++)
 		{
 			if (j >= 10)
-				putchar(j / 10 + '0');
-			putchar(j % 10 + '0');
+				_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
 		}
 		_putchar('\n');
 	}
