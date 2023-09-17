9b4d958277662aef6a808161a2491347e5f5ce51
youssefAbooooo <youssef@example.com>
2023-09-17 08:05:43 -0700

    kk

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - Entry point
+ *
+ * Description: _ilower  want to check if it is lowercase or not
+ *
+ * Return: Always 0 (Success)
+ */
+void print_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
