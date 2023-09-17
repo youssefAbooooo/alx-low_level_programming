47d75867f4077fb9818635ece08acc7f78f72d56
youssefAbooooo <youssef@example.com>
2023-09-17 08:26:30 -0700

    kj

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * more_numbers - Entry point
+ *
+ * Description: _ilower  want to check if it is lowercase or not
+ * @c: this a number
+ *
+ * Return: Always 0 (Success)
+ */
+
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		int j;
+
+		for (j = 0; j < 15; j++)
+		{
+			if (j < 10)
+				_putchar(j + '0');
+			else
+			{
+				_putchar(j / 10 + '0');
+				_putchar(j % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
