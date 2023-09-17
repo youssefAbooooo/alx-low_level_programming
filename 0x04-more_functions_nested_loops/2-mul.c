f48a793f5bb92412ab0be28ba172fd13d2e8f3d3
youssefAbooooo <youssef@example.com>
2023-09-17 07:52:27 -0700

    dfd

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - Entry point
+ *
+ * Description: _ilower  want to check if it is lowercase or not
+ * @c: this a number
+ *
+ * Return: Always 0 (Success)
+ */
+
+int mul(int a, int b)
+{
+	return(a * b);
+}
