a5050619988bc2a347a8cf820520edc71247411e
youssefAbooooo <youssef@example.com>
2023-09-17 07:56:26 -0700

    dfd

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,12 +4,13 @@
  * mul - Entry point
  *
  * Description: _ilower  want to check if it is lowercase or not
- * @c: this a number
+ * @a: this a number
+ * @b: this a number
  *
  * Return: Always 0 (Success)
  */
 
 int mul(int a, int b)
 {
-	return(a * b);
+	return (a * b);
 }
