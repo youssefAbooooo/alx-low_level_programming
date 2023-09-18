2c46c2f0aee64b04660bf9d5c44e5f11cb638980
youssefAbooooo <youssef@example.com>
2023-09-18 11:05:24 -0700

    jj

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts- Entry point
+ *
+ * Description: _ilower  want to check if it is lowercase or not
+ * @str: this a string
+ *
+ * Return: Always 0 (Success)
+ */
+
+void _puts(char *str)
+{
+	while (*s != '\0')
+	{
+		_putchar(*s++);
+	}
+	_putchar('\n');
+}
