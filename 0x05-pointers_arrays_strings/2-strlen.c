6307cc06c7a8fb7ff154c8824fc90eb66c913bfa
youssefAbooooo <youssef@example.com>
2023-09-18 10:49:33 -0700

    jj

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - Entry point
+ *
+ * Description: _ilower  want to check if it is lowercase or not
+ * @s: this a number
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(*s));
+}
