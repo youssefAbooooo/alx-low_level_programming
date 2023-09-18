fea8f31f4cf440b3a0a2f29af68547b513a9e9be
youssefAbooooo <youssef@example.com>
2023-09-18 10:40:14 -0700

    j

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Entry point
+ *
+ * Description: _ilower  want to check if it is lowercase or not
+ * @n: this a number
+ *
+ * Return: Always 0 (Success)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
