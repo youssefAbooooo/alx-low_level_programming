ff9c192515de61ed9aeabd870723cc6430e220c4
youssefAbooooo <youssef@example.com>
2023-09-18 11:25:46 -0700

    jj

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev- Entry point
+ *
+ * Description: _ilower  want to check if it is lowercase or not
+ * @: this a number
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_rev(char *s)
+{
+	int len = _strlen(s);
+	while (len)
+	{
+		_putchar(*(s + len - 1));
+		len--;
+	}
+	_putchar('\n');
+}
